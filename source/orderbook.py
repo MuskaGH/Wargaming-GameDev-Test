66701af6c0e7a483de08c43ece964b089f7cf41e
Martin Musil <martinmusildev@gmail.com>
2024-02-13 20:35:46 +0100

    Patch 0.1.0.
    - Created 'OrderBook' class with a basic structure
    - Added 'input_data.txt' that will hold the data
    - Now thinking about the best way to implement the logic without heavy performance costs (currently researching heap queue algorithm that seems like a nice fit)

---
+++ source/orderbook.py
@@ -0,0 +1,15 @@
+import heapq
+
+class OrderBook:
+    def __init__(self):
+        self.active_orders = {} 
+        self.current_max_price = []
+
+    def get_current_max_price(self):
+        pass
+
+    def add_order(self, id, timestamp, price):
+        pass
+        
+    def remove_order(self, id):
+        pass
